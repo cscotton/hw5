f88e184fd36b30f7bf00366d4142776c5705e923
Chris Cotton <chris-cotton@uiowa.edu>
2013-04-01 21:44:25 -0500

    role based security is enabled and working correctly

---
+++ login/default.aspx.vb
@@ -0,0 +1,12 @@
+﻿
+Partial Class login_default
+    Inherits System.Web.UI.Page
+
+
+    'have cursor focus set to username on page load
+    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+
+        Login1.Focus()
+
+    End Sub
+End Class
